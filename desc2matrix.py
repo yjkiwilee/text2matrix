d8f4285bec03a84983a213929bc51e4e712d3d7d
Young Jun Lee <yjkiwilee@gmail.com>
2024-07-17 14:25:52 +0100

    Edited desc2matrix.py so that it can take in description files produced by dwca2csv.py

--- desc2matrix.py
+++ desc2matrix.py
@@ -2,6 +2,7 @@ import argparse
 import json
 from ollama import Client
 import os
+import pandas as pd
 
 def desc2dict(sys_prompt, prompt, descriptions, client, model = 'desc2matrix'):
     # Pass descriptions to LLM for response
@@ -59,68 +60,87 @@ def main():
     parser = argparse.ArgumentParser(description = 'Extract JSON/dict from description files')
 
     # Add the arguments
-    parser.add_argument('descfile', type = str, help = 'File containing the descriptions separated by newline')
+    parser.add_argument('descfile', type = str, help = 'File containing the descriptions produced by dwca2csv.py')
     parser.add_argument('outputfile', type = str, help = 'File to write JSON to')
     parser.add_argument('--mode', required = True, type = str, choices = ['desc2dict', 'desc2list2dict'], help = 'Transcription mode to use')
+    parser.add_argument('--desctype', required = True, type = str, help = 'The "type" value used for morphological descriptions in the description file')
+    parser.add_argument('--start', required = False, type = int, default = 0, help = 'Order ID of the species to start transcribing from')
+    parser.add_argument('--spnum', required = False, type = int, help = 'Number of species to process descriptions of. Default behaviour is to process all species present in the file')
     parser.add_argument('--model', required = False, type = str, default = 'llama3', help = 'Name of base LLM to use')
     parser.add_argument('--temperature', required = False, type = float, default = 0.1, help = 'Model temperature between 0 and 1')
     parser.add_argument('--seed', required = False, type = int, default = 0, help = 'Model seed value')
     parser.add_argument('--promptsdir', required = False, type = str, default = './prompts', help = 'Folder storing the prompt files')
+    
 
     # Parse the arguments
     args = parser.parse_args()
 
-    # Open descfile
-    with open(args.descfile, 'r') as descfile:
-        # Read relevant files
-        descs = descfile.read().split('\n')
+    # Read descfile
+    descdf = pd.read_csv(args.descfile, sep = '\t')
 
-        # Build params
-        params = {'temperature': args.temperature, 'seed': args.seed}
+    # Filter morphological descriptions only
+    descdf = descdf.loc[descdf['type'] == args.desctype]
 
-        # Build modelfile
-        modelfile = '{}\n{}'.format(
-            'FROM {}'.format(args.model),
-            '\n'.join(['PARAMETER {} {}'.format(param, value) for param, value in params.items()])
-        )
+    # Slice according to --start and --spnum options
+    if(args.spnum != None):
+        descdf = descdf[args.start : args.start + args.spnum]
 
-        # Make connection to client
-        client = Client(host = 'http://localhost:11434')
+    # Extract descriptions
+    descs = descdf['description'].tolist()
 
-        # Create model with the specified params
-        client.create(model = 'desc2matrix', modelfile = modelfile)
+    # Build params
+    params = {'temperature': args.temperature, 'seed': args.seed}
 
-        # Check given transcription mode
-        if(args.mode == 'desc2dict'):
+    # Build modelfile
+    modelfile = '{}\n{}'.format(
+        'FROM {}'.format(args.model),
+        '\n'.join(['PARAMETER {} {}'.format(param, value) for param, value in params.items()])
+    )
 
-            # Open & read sys_prompt.txt and prompt.txt
-            prompt_fnames = ['sys_prompt.txt', 'prompt.txt']
-            prompt_ftypes = ['sys_prompt', 'prompt']
-            prompts = {}
+    # Make connection to client
+    client = Client(host = 'http://localhost:11434')
 
-            for fname, ftype in zip(prompt_fnames, prompt_ftypes):
-                with open(os.path.join(args.promptsdir, fname), 'r') as fp:
-                    prompts[ftype] = fp.read()
+    # Create model with the specified params
+    client.create(model = 'desc2matrix', modelfile = modelfile)
 
-            descdict = desc2dict(prompts['sys_prompt'], prompts['prompt'], descs, client)
+    # Check given transcription mode
+    if(args.mode == 'desc2dict'):
 
-        elif(args.mode == 'desc2list2dict'):
+        # Open & read sys_prompt.txt and prompt.txt
+        prompt_fnames = ['sys_prompt.txt', 'prompt.txt']
+        prompt_ftypes = ['sys_prompt', 'prompt']
+        prompts = {}
 
-            # Open & read d2l_sys_prompt.txt, d2l_prompt.txt, l2d_sys_prompt.txt, l2d_prompt.txt
-            prompt_fnames = ['d2l_sys_prompt.txt', 'd2l_prompt.txt', 'l2d_sys_prompt.txt', 'l2d_prompt.txt']
-            prompt_ftypes = ['d2l_sys_prompt', 'd2l_prompt', 'l2d_sys_prompt', 'l2d_prompt']
-            prompts = {}
+        for fname, ftype in zip(prompt_fnames, prompt_ftypes):
+            with open(os.path.join(args.promptsdir, fname), 'r') as fp:
+                prompts[ftype] = fp.read()
 
-            for fname, ftype in zip(prompt_fnames, prompt_ftypes):
-                with open(os.path.join(args.promptsdir, fname), 'r') as fp:
-                    prompts[ftype] = fp.read()
-            
-            desclists = desc2list(prompts['d2l_sys_prompt'], prompts['d2l_prompt'], descs, client)
-            descdict = list2dict(prompts['l2d_sys_prompt'], prompts['l2d_prompt'], desclists, client)
+        descdict = desc2dict(prompts['sys_prompt'], prompts['prompt'], descs, client)
+
+    elif(args.mode == 'desc2list2dict'):
+
+        # Open & read d2l_sys_prompt.txt, d2l_prompt.txt, l2d_sys_prompt.txt, l2d_prompt.txt
+        prompt_fnames = ['d2l_sys_prompt.txt', 'd2l_prompt.txt', 'l2d_sys_prompt.txt', 'l2d_prompt.txt']
+        prompt_ftypes = ['d2l_sys_prompt', 'd2l_prompt', 'l2d_sys_prompt', 'l2d_prompt']
+        prompts = {}
+
+        for fname, ftype in zip(prompt_fnames, prompt_ftypes):
+            with open(os.path.join(args.promptsdir, fname), 'r') as fp:
+                prompts[ftype] = fp.read()
         
-        # Write dict as json
-        with open(args.outputfile, 'w') as outfile:
-            json.dump(descdict, outfile)
+        desclists = desc2list(prompts['d2l_sys_prompt'], prompts['d2l_prompt'], descs, client)
+        descdict = list2dict(prompts['l2d_sys_prompt'], prompts['l2d_prompt'], desclists, client)
+    
+    # Build output JSON
+    outdict = [{
+        'coreid': descdf.iloc[rowid]['coreid'],
+        'original_description': descdf.iloc[rowid]['description'],
+        'char_json': dd
+    } for rowid, dd in enumerate(descdict)]
+
+    # Write output as JSON
+    with open(args.outputfile, 'w') as outfile:
+        json.dump(outdict, outfile)
 
 if __name__ == '__main__':
     main()
