6ca52787fce213dfd58c4932b965266b734d1840
Young Jun Lee <yjkiwilee@gmail.com>
2024-07-17 17:32:24 +0100

    Developed test pipeline for getting word coverage from a desc2matrix.py output file

--- desc2matrix_qc.py
+++ desc2matrix_qc.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 import json
 import nltk
 
@@ -6,15 +7,53 @@ nltk.download('punkt')
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 
-# Tokenize words using NLTK
-stop_words = set(stopwords.words('english'))
+def get_word_set(descstr):
+    # Gather stop words
+    stop_words = set(stopwords.words('english'))
+
+    # Insert whitespace after period, comma, colon and semicolon
+    descstr = re.sub(r'[^0-9] *\. *[^0-9]', '. ', descstr) # Do not substitute periods in floating-point numbers
+    descstr = re.sub(r' *, *', ', ', descstr)
+    descstr = re.sub(r' *: *', ': ', descstr)
+    descstr = re.sub(r' *; *', '; ', descstr)
+
+    # Collapse numeric ranges to single 'word' to check for presence
+    descstr = re.sub(r'([0-9]) *- * ([0-9])', r'\1-\2', descstr)
+
+    # Tokenise words
+    descset = set([w for w in nltk.word_tokenize(descstr) if not w.lower() in stop_words])
+
+    # Remove punctuations post-tokenisation
+    descset = descset.difference({'.', ',', ':', ';', '“', '”', '"', "'"})
+
+    # Return word set
+    return descset
 
 # Read output from desc2matrix.py
+desc_output = {}
 
-desc_words_set = set([w for w in nltk.word_tokenize(desc) if not w.lower() in stop_words]) 
-desc_formatted_words_set = set([w for w in nltk.word_tokenize(desc_formatted) if not w.lower() in stop_words]  )
+with open('test_scripts/test_output.json', 'r') as fp:
+    desc_output = json.loads(fp.read())
+
+# Extract original descriptions and their JSON counterparts
+descs = [desc['original_description'] for desc in desc_output]
+descdicts = [desc['char_json'] for desc in desc_output]
+
+# Merge characteristics in descdicts into single strings
+descs_formatted = ['\n'.join(['{}: {}'.format(char['characteristic'], char['value']) for char in descdict])
+                   for descdict in descdicts]
+
+# Get word sets
+descset = [get_word_set(desc) for desc in descs][0]
+descset_formatted = [get_word_set(desc_f) for desc_f in descs_formatted][0]
 
 # Calculate exact word match proportion
-common_words = desc_words_set.intersection(desc_formatted_words_set)
-word_match_proportion = len(common_words) / len(desc_words_set)
+common_words = descset.intersection(descset_formatted)
+original_only = descset.difference(descset_formatted)
+formatted_only = descset_formatted.difference(descset)
+word_match_proportion = len(common_words) / len(descset)
+
 print(f'Proportion of Words Matched: {word_match_proportion:.2f}')
+print('Common words:\n{}'.format(', '.join(common_words)))
+print('Words in the original description only:\n{}'.format(', '.join(original_only)))
+print('Words in the formatted descrtiption only:\n{}'.format(', '.join(formatted_only)))
