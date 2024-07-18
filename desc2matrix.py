c92c6a743e71d22052fbc63e6c5edb41c0c0f49b
Young Jun Lee <yjkiwilee@gmail.com>
2024-07-18 12:40:29 +0100

    Edited desc2matrix.py to improve code readability

--- desc2matrix.py
+++ desc2matrix.py
@@ -5,15 +5,15 @@ import os
 import pandas as pd
 import time
 
-def regularise_dict(dict):
+def regularise_charjson(chars):
     # Return false if the object is not a list; we expect a list of {'characteristic':'', 'value':''}
-    if not isinstance(dict, list):
+    if not isinstance(chars, list):
         return False
 
     new_dict = []
 
     # Go through elements
-    for char in dict:
+    for char in chars:
         # Return false if the keys are different from what we expect
         if set(char.keys()) != {'characteristic', 'value'}:
             return False
@@ -31,48 +31,48 @@ def regularise_dict(dict):
     # Return the new dict
     return new_dict
 
-def desc2dict(sys_prompt, prompt, descriptions, client, model = 'desc2matrix', silent = False):
+def desc2charjson(sys_prompt, prompt, descs, client, model = 'desc2matrix', silent = False):
     # Pass descriptions to LLM for response
-    desc_dicts = []
+    char_jsons = []
 
-    for i, description in enumerate(descriptions):
+    for i, desc in enumerate(descs):
         start = 0
         if not silent:
-            print('desc2dict: processing {}/{}... '.format(i+1, len(descriptions)), end = '', flush = True)
+            print('desc2json: processing {}/{}... '.format(i+1, len(descs)), end = '', flush = True)
             start = time.time()
 
         # Generate response while specifying system prompt
-        response = client.generate(model = model,
-                                   prompt = prompt + '\n' + description,
+        resp = client.generate(model = model,
+                                   prompt = prompt + '\n' + desc,
                                    system = sys_prompt)['response']
 
         # Attempt to parse prompt as JSON
         try:
-            response_dict = json.loads(response)
+            resp_json = json.loads(resp)
             # Check validity / regularise output
-            reg_response_dict = regularise_dict(response_dict)
-            if reg_response_dict != False:
-                desc_dicts.append(reg_response_dict)
+            reg_resp_json = regularise_charjson(resp_json)
+            if reg_resp_json != False:
+                char_jsons.append(reg_resp_json)
             else:
                 if not silent:
-                    print('Ollama output is JSON but is structured badly:\n{}'.format(str(response_dict)))
-                desc_dicts.append(None) # Append None == null if not valid
+                    print('Ollama output is JSON but is structured badly:\n{}'.format(str(resp_json)))
+                char_jsons.append(None) # Append None == null if not valid
         except json.decoder.JSONDecodeError as decode_err: # If LLM returns bad string
             if not silent:
-                print('Ollama returned bad JSON string:\n{}'.format(response))
-            desc_dicts.append(None) # Append None == null
+                print('Ollama returned bad JSON string:\n{}'.format(resp))
+            char_jsons.append(None) # Append None == null
         
         if not silent:
             elapsed_t = time.time() - start
             print(f'done in {elapsed_t:.2f} s!')
     
     # Return characteristics as array of dict
-    return desc_dicts
+    return char_jsons
 
-def desc2list2dict(d2l_sys_prompt, d2l_prompt, l2d_sys_prompt, l2d_prompt, descs, client, model = 'desc2matrix', silent = False):
+def desc2list2charjson(d2l_sys_prompt, d2l_prompt, l2j_sys_prompt, l2j_prompt, descs, client, model = 'desc2matrix', silent = False):
     # List to store intermediate lists and final JSONs
     desc_lists = []
-    desc_dicts = []
+    char_jsons = []
 
     # Pass descriptions to LLM for response
     for i, desc in enumerate(descs):
@@ -94,36 +94,36 @@ def desc2list2dict(d2l_sys_prompt, d2l_prompt, l2d_sys_prompt, l2d_prompt, descs
         if not silent:
             elapsed_t = time.time() - start
             print(f'done in {elapsed_t:.2f} s!')
-            print('list2dict: processing {}/{}... '.format(i+1, len(descs)), end = '', flush = True)
+            print('list2json: processing {}/{}... '.format(i+1, len(descs)), end = '', flush = True)
             start = time.time()
         
         # Generate JSON response
-        json_resp = client.generate(model = 'desc2matrix',
-                            prompt = l2d_prompt + '\n' + liststr,
-                            system = l2d_sys_prompt)['response']
+        resp = client.generate(model = 'desc2matrix',
+                            prompt = l2j_prompt + '\n' + liststr,
+                            system = l2j_sys_prompt)['response']
         
-        # Attempt to parse response to dict
+        # Attempt to parse response to JSON
         try:
-            resp_dict = json.loads(json_resp)
-            # Check validity / regularise dict
-            reg_resp_dict = regularise_dict(resp_dict)
-            if reg_resp_dict != False:
-                desc_dicts.append(reg_resp_dict)
+            resp_json = json.loads(resp)
+            # Check validity / regularise JSON
+            reg_resp_json = regularise_charjson(resp_json)
+            if reg_resp_json != False: # If JSON valid
+                char_jsons.append(reg_resp_json)
             else:
                 if not silent:
-                    print('Ollama output is JSON but is structured badly:\n{}'.format(str(resp_dict)))
-                desc_dicts.append(None) # Append None == null if not valid
+                    print('Ollama output is JSON but is structured badly:\n{}'.format(str(resp_json)))
+                char_jsons.append(None) # Append None == null if not valid
         except json.decoder.JSONDecodeError as decode_err: # If LLM returns bad string
             if not silent:
-                print('Ollama returned bad JSON string:\n{}'.format(json_resp))
-            desc_dicts.append(None) # Append None == null
+                print('Ollama returned bad JSON string:\n{}'.format(resp_json))
+            char_jsons.append(None) # Append None == null
         
         if not silent:
             elapsed_t = time.time() - start
             print(f'done in {elapsed_t:.2f} s!')
     
-    # Return desc_lists and desc_dicts as a dictionary
-    return {'lists': desc_lists, 'dicts': desc_dicts}
+    # Return desc_lists and char_jsons as dictionary
+    return {'lists': desc_lists, 'jsons': char_jsons}
 
 def main():
     # Create the parser
@@ -132,7 +132,7 @@ def main():
     # Add the arguments
     parser.add_argument('descfile', type = str, help = 'File containing the descriptions produced by dwca2csv.py')
     parser.add_argument('outputfile', type = str, help = 'File to write JSON to')
-    parser.add_argument('--mode', required = True, type = str, choices = ['desc2dict', 'desc2list2dict'], help = 'Transcription mode to use')
+    parser.add_argument('--mode', required = True, type = str, choices = ['desc2json', 'desc2list2json'], help = 'Transcription mode to use')
     parser.add_argument('--desctype', required = True, type = str, help = 'The "type" value used for morphological descriptions in the description file')
     parser.add_argument('--start', required = False, type = int, default = 0, help = 'Order ID of the species to start transcribing from')
     parser.add_argument('--spnum', required = False, type = int, help = 'Number of species to process descriptions of. Default behaviour is to process all species present in the file')
@@ -176,7 +176,7 @@ def main():
     outdict = None
 
     # Check given transcription mode
-    if(args.mode == 'desc2dict'):
+    if(args.mode == 'desc2json'):
 
         # Open & read sys_prompt.txt and prompt.txt
         prompt_fnames = ['sys_prompt.txt', 'prompt.txt']
@@ -187,37 +187,37 @@ def main():
             with open(os.path.join(args.promptsdir, fname), 'r') as fp:
                 prompts[ftype] = fp.read()
 
-        desc_dicts = desc2dict(prompts['sys_prompt'], prompts['prompt'], descs, client, silent = args.silent == True)
+        char_jsons = desc2charjson(prompts['sys_prompt'], prompts['prompt'], descs, client, silent = args.silent == True)
 
         # Build output JSON
         outdict = [{
             'coreid': descdf.iloc[rowid]['coreid'],
             'original_description': descdf.iloc[rowid]['description'],
-            'char_json': dd
-        } for rowid, dd in enumerate(desc_dicts)]
+            'char_json': cj
+        } for rowid, cj in enumerate(char_jsons)]
 
-    elif(args.mode == 'desc2list2dict'):
+    elif(args.mode == 'desc2list2json'):
 
         # Open & read d2l_sys_prompt.txt, d2l_prompt.txt, l2d_sys_prompt.txt, l2d_prompt.txt
-        prompt_fnames = ['d2l_sys_prompt.txt', 'd2l_prompt.txt', 'l2d_sys_prompt.txt', 'l2d_prompt.txt']
-        prompt_ftypes = ['d2l_sys_prompt', 'd2l_prompt', 'l2d_sys_prompt', 'l2d_prompt']
+        prompt_fnames = ['d2l_sys_prompt.txt', 'd2l_prompt.txt', 'l2j_sys_prompt.txt', 'l2j_prompt.txt']
+        prompt_ftypes = ['d2l_sys_prompt', 'd2l_prompt', 'l2j_sys_prompt', 'l2j_prompt']
         prompts = {}
 
         for fname, ftype in zip(prompt_fnames, prompt_ftypes):
             with open(os.path.join(args.promptsdir, fname), 'r') as fp:
                 prompts[ftype] = fp.read()
         
-        desc_outs = desc2list2dict(prompts['d2l_sys_prompt'], prompts['d2l_prompt'],
-                                   prompts['l2d_sys_prompt'], prompts['l2d_prompt'],
+        desc_outs = desc2list2charjson(prompts['d2l_sys_prompt'], prompts['d2l_prompt'],
+                                   prompts['l2j_sys_prompt'], prompts['l2j_prompt'],
                                    descs, client, silent = args.silent == True)
 
         # Build output JSON
         outdict = [{
             'coreid': descdf.iloc[rowid]['coreid'],
             'original_description': descdf.iloc[rowid]['description'],
-            'char_list': dl,
-            'char_json': dd
-        } for rowid, dl, dd in zip(list(range(0, len(desc_outs))), desc_outs['lists'], desc_outs['dicts'])]
+            'char_list': cl,
+            'char_json': cj
+        } for rowid, cl, cj in zip(list(range(0, len(desc_outs['jsons']))), desc_outs['lists'], desc_outs['jsons'])]
 
     # Write output as JSON
     with open(args.outputfile, 'w') as outfile:
