dff59c8c71616cfd5b315156f714c472386c1244
Young Jun Lee <yjkiwilee@gmail.com>
2024-07-24 17:50:47 +0100

    Reduced context size and developing accumulation_curve.R

--- data_visualisation/accumulation_curve.R
+++ data_visualisation/accumulation_curve.R
@@ -6,3 +6,23 @@ pacman::p_load("tidyverse", "here", "jsonlite")
 
 # Load json
 charjson_dat <- read_json(here("accum_out.json"))
+
+# ===== Generate accumulation curve =====
+
+# Get charlist history and charlist length history
+charlist_hist <- charjson_dat$charlist_history
+charlistlen_hist <- charjson_dat$charlist_len_history
+
+# Build tibble
+accum_df <- tibble(
+  sp_i = seq(1, length(charlistlen_hist)),
+  charlist_len = as.numeric(charlistlen_hist)
+)
+
+# Plot accumulation curve
+accum_plt <- ggplot(accum_df, aes(x = sp_i, y = charlist_len)) +
+  geom_line() +
+  theme_bw() +
+  scale_y_continuous(limits = c(0, 50))
+accum_plt
+ggsave(here("figures/accum_curve.png"), accum_plt, width = 4, height = 3)
