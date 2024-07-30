d7a975f403e73339caa3632cc161c830f266c985
Young Jun Lee <yjkiwilee@gmail.com>
2024-07-30 11:47:12 +0100

    Update shell scripts

--- generate_json.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source env/Scripts/activate
-python desc2matrix_accum.py data/solanaceae-desc.txt json_outputs/accum_out.json --desctype=general --spnum=100 
-python desc2matrix_accum_tab.py data/solanaceae-desc.txt json_outputs/accum_tab_out.json --desctype=general --spnum=100
-deactivate
