6aaf203489a26644f3c182b7402b21d70ba6648f
Young Jun Lee <yjkiwilee@gmail.com>
2024-07-30 10:47:59 +0100

    Add code to generate histogram for the proportion of words 'created'

--- data_visualisation/qc_comparison.R
+++ data_visualisation/qc_comparison.R
@@ -25,7 +25,8 @@ accum_f_qc <- read_tsv(here::here("../json_outputs/accum_followup_800sp_qc.tsv")
 
 # Merge dataframes
 qc_df <- rbind(accum_qc, accum_tab_qc, accum_f_qc) %>%
-  filter(status == "success") # Filter only succeeded runs
+  filter(status == "success") %>% # Filter only succeeded runs
+  mutate(prop_created = nwords_created / nwords_result) # Create column for the proportion of recovered words that were 'created'
 
 # ===== Generate QC plots =====
 
@@ -47,3 +48,20 @@ qc_hist <- ggplot(qc_df, aes(x = prop_recovered)) +
   theme_bw()
 qc_hist
 ggsave(here::here("figures/prop_recovered_histogram.png"), qc_hist, width = 5, height = 4)
+
+# ===== Generate plots for the proportion of words 'created' =====
+
+# Histogram of word coverage
+qc_created_hist <- ggplot(qc_df, aes(x = prop_created)) +
+  geom_histogram(binwidth = 0.05, fill = "#ffffff", color = "#000000", boundary = 0) +
+  geom_vline(data = ddply(qc_df, "mode", summarize, med_prop = median(prop_created)),
+             aes(xintercept = med_prop), linetype = "dashed") +
+  facet_wrap(~mode, ncol = 1, labeller = labeller(mode = mode_lab)) +
+  scale_x_continuous(breaks = seq(0, 1, by = 0.1)) +
+  labs(
+    x = "Proportion of recovered words not in the original description",
+    y = "Count"
+  ) +
+  theme_bw()
+qc_created_hist
+ggsave(here::here("figures/prop_created_histogram.png"), qc_created_hist, width = 5, height = 4)
