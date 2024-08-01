cec2517e750b6a6a6e5ed3a2494db72070e06460
Young Jun Lee <yjkiwilee@gmail.com>
2024-08-01 11:15:48 +0100

    Add script for runing wcharlist

--- generate_json_wcharlist.sh
+++ generate_json_wcharlist.sh
@@ -2,6 +2,6 @@
 source env/Scripts/activate
 # python desc2matrix_wcharlist.py data/solanaceae-desc.txt charlists/charlist_solanaceae.txt outputs/wcharlist_out_800sp.json --desctype=general --spnum=800
 # python desc2matrix_wcharlist_followup.py data/solanaceae-desc.txt charlists/charlist_solanaceae.txt outputs/wcharlist_followup_out_800sp.json --desctype=general --spnum=800
-python desc2matrix_wcharlist.py data/solanaceae-desc.txt charlists/charlist_solanaceae_shorter.txt outputs/wcharlist_shortlist_out_800sp.json --desctype=general --spnum=800
+# python desc2matrix_wcharlist.py data/solanaceae-desc.txt charlists/charlist_solanaceae_shorter.txt outputs/wcharlist_shortlist_out_800sp.json --desctype=general --spnum=800
 python desc2matrix_wcharlist_followup.py data/solanaceae-desc.txt charlists/charlist_solanaceae_shorter.txt outputs/wcharlist_shortlist_followup_out_800sp.json --desctype=general --spnum=800
 deactivate
