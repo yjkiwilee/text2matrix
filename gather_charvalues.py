a7ac275cc740fa87099012e956c7b15ca1c9357a
Young Jun Lee <yjkiwilee@gmail.com>
2024-08-01 12:57:33 +0100

    Create gather_charvalues.py for summarising characteristics and their possible values given a JSON output

---
+++ gather_charvalues.py
@@ -0,0 +1,6 @@
+"""
+This python script organises the values for each characteristic that have been recovered by the
+desc2matrix_wcharlist_*.py scripts, generating a table of the possible values that each
+characteristics can take within the dataset and their frequency of occurrence.
+"""
+
