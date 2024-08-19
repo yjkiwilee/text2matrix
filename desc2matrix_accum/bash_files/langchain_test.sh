b27e941ddbf9f7b8d308b00785ad290455ae7112
Young Jun Lee <yjkiwilee@gmail.com>
2024-08-19 09:46:42 +0100

    Create bash file to run LangChain scripts

---
+++ desc2matrix_accum/bash_files/langchain_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script is for running desc2matrix on the descriptions
+# that were subsetting to only include those within the Solanum Key: https://app.xper3.fr/xper3GeneratedFiles/publish/identification/-3915026624309343770/mkey.html
+
+# Go to main branch directory
+cd ..
+# Activate venv
+# source ../env/Scripts/activate
+# Run desc2matrix_accum scripts
+python desc2matrix_langchain_accum.py ../data/solanaceae-desc-subset.txt outputs/accum_output/accum_langchain_subset.json --desctype=general
+
+python desc2matrix_langchain_wcharlist.py ../data/solanaceae-desc-subset.txt charlists/solanum_charlist.txt outputs/wcharlist_output/wcharlist_langchain_subset.json --desctype=general --charlistsep="; "
+
+python desc2matrix_langchain_wcharlist.py ../data/solanaceae-desc-subset.txt charlists/solanum_charlist_gen_shorter.txt outputs/wcharlist_output/wcharlist_langchain_sgenlist_subset.json --desctype=general --charlistsep="; "
+
+python desc2matrix_langchain_wcharlist.py ../data/solanaceae-desc-subset.txt charlists/solanum_charlist_gen_longer.txt outputs/wcharlist_output/wcharlist_langchain_lgenlist_subset.json --desctype=general --charlistsep="; "
+
+# Deactivate venv
+# deactivate
