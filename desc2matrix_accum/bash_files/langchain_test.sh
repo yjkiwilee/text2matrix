909406dff7e3c0dcda3c5d2344a2626a9af82bf7
Young Jun Lee <yjkiwilee@gmail.com>
2024-08-20 14:02:16 +0100

    Modify bash script for generating LangChain outputs

--- desc2matrix_accum/bash_files/langchain_test.sh
+++ desc2matrix_accum/bash_files/langchain_test.sh
@@ -8,13 +8,13 @@ cd ..
 # Activate venv
 # source ../env/Scripts/activate
 # Run desc2matrix_accum scripts
-python desc2matrix_langchain_accum.py ../data/solanaceae-desc-subset.txt outputs/accum_output/accum_langchain_subset.json --desctype=general
+# python desc2matrix_langchain_accum.py ../data/solanaceae-desc-subset.txt outputs/accum_output/accum_langchain_subset.json --desctype=general
 
-python desc2matrix_langchain_wcharlist.py ../data/solanaceae-desc-subset.txt charlists/solanum_charlist.txt outputs/wcharlist_output/wcharlist_langchain_subset.json --desctype=general --charlistsep="; "
+python desc2matrix_langchain_wcharlist.py ../data/solanaceae-desc-subset.txt charlists/solanum_charlist.txt outputs/wcharlist_output/wcharlist_langchain_subset_2nd.json --desctype=general --charlistsep="; " --temperature=0
 
-python desc2matrix_langchain_wcharlist.py ../data/solanaceae-desc-subset.txt charlists/solanum_charlist_gen_shorter.txt outputs/wcharlist_output/wcharlist_langchain_sgenlist_subset.json --desctype=general --charlistsep="; "
+python desc2matrix_langchain_wcharlist.py ../data/solanaceae-desc-subset.txt charlists/solanum_charlist_gen_shorter.txt outputs/wcharlist_output/wcharlist_langchain_sgenlist_subset_2nd.json --desctype=general --charlistsep="; " --temperature=0
 
-python desc2matrix_langchain_wcharlist.py ../data/solanaceae-desc-subset.txt charlists/solanum_charlist_gen_longer.txt outputs/wcharlist_output/wcharlist_langchain_lgenlist_subset.json --desctype=general --charlistsep="; "
+python desc2matrix_langchain_wcharlist.py ../data/solanaceae-desc-subset.txt charlists/solanum_charlist_gen_longer.txt outputs/wcharlist_output/wcharlist_langchain_lgenlist_subset_2nd.json --desctype=general --charlistsep="; " --temperature=0
 
 # Deactivate venv
 # deactivate
