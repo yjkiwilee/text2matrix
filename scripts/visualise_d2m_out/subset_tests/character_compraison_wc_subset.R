061a85e8d30adaad68a01aa2a2a835d215d0e95d
Young Jun Lee <yjkiwilee@gmail.com>
2024-08-21 15:25:15 +0100

    Edit bash script

--- scripts/visualise_d2m_out/subset_tests/character_compraison_wc_subset.R
+++ scripts/visualise_d2m_out/subset_tests/character_compraison_wc_subset.R
@@ -11,13 +11,19 @@ pacman::p_load("tidyverse", "here", "plyr")
 # Run names
 run_names <- c(
   "wcharlist",
-  "wcharlist_f"
+  "wcharlist_sgenlist",
+  "wcharlist_lgenlist",
+  "wcharlist_f",
+  "wcharlist_f_sgenlist",
+  "wcharlist_f_lgenlist"
 )
 names(run_names) <- run_names
 
 # Run labels
 run_labels <- c(
-  "Without follow-up questions",
+  "Characteristics in key",
+  "Short list of characteristics extracted by LLM",
+  "Long list of characteristics extracted by LLM",
   "With follow-up questions"
 )
 
@@ -32,7 +38,10 @@ run_dfs <- lapply(seq_along(tsv_paths), function(i) {
   run_name <- names(tsv_paths)[[i]]
   read_tsv(here::here(tsv_path)) %>%
     select(-1) %>% # Remove first column
-    mutate(run_name = run_name) # Append column indicating run name
+    mutate(
+      run_name = run_name, # Append column indicating run name
+      status = factor(status, levels = c("success", "invalid_json", "invalid_json_followup"))
+    ) 
 })
 
 # Bind into single dataframe
@@ -41,7 +50,30 @@ runs_df <- bind_rows(run_dfs)
 # ===== Run status plots =====
 
 status_barplot <- ggplot(runs_df, aes(y = run_name, fill = status)) +
-  geom_bar()
+  geom_bar(width = .5) +
+  scale_y_discrete(
+    labels = run_labels
+  ) +
+  scale_x_continuous(
+    breaks = seq(0, 500, 100)
+  ) +
+  scale_fill_manual(
+    labels = c("Success", "Invalid JSON output in initial prompt", "Invalid JSON output in follow-up prompt"),
+    values = c("#abf2a7", "#facc16", "#ed1c09"),
+    breaks = c("success", "invalid_json", "invalid_json_followup")
+  ) +
+  labs(
+    x = "Run",
+    y = "Count",
+    fill = "Status"
+  ) +
+  theme_classic() +
+  theme(
+    legend.position = "bottom",
+    panel.grid.major.y = element_line(color = "lightgrey", linewidth = 0.5),
+    panel.grid.minor.y = element_line(color = "lightgrey", linewidth = 0.25)
+  ) +
+  guides(fill = guide_legend(ncol=1,byrow=TRUE))
 
 status_barplot
-
+ggsave(here::here("../../script_output/visualise_d2m_out/poster/status.png"), status_barplot, width = 4, height = 5)
